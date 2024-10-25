819cfea22a693c035e22b180260ffb3e3fd08f1d
pietro <pietro.ciani@edu.unifi.it>
2024-10-25 19:09:14 +0200

    lombok

--- src/main/java/com/example/springSWE/model/User.java
+++ src/main/java/com/example/springSWE/model/User.java
@@ -1,9 +1,13 @@
 package com.example.springSWE.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="users")
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -17,38 +21,6 @@ public class User {
     private String email;
 
     private String password;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-       this.password = password;
-    }
 
     @Override
     public String toString() {
