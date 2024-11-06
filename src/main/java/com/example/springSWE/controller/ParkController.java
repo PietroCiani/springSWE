c90ba21a367a957e5745d944d9fe3f3c3a6601fe
pietro <pietro.ciani@edu.unifi.it>
2024-11-06 18:50:18 +0100

    style(res): name park selection section

--- src/main/java/com/example/springSWE/controller/ParkController.java
+++ src/main/java/com/example/springSWE/controller/ParkController.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Controller
 public class ParkController {
