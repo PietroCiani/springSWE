ce66e44881b9e871fb9769a080ca76c7b041027e
pietro <pietro.ciani@edu.unifi.it>
2024-11-18 11:26:23 +0100

    test(init): add test lib and first test structure

---
+++ src/test/java/com/example/springSWE/service/ReservationServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.springSWE.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ReservationServiceTest {
+	@Test
+	void testDeleteReservation() {
+
+	}
+
+	@Test
+	void testFindReservationsForParkAndDateWithinTimeRange() {
+
+	}
+
+	@Test
+	void testGetAllReservationsByPark() {
+
+	}
+
+	@Test
+	void testGetFutureReservations() {
+
+	}
+
+	@Test
+	void testGetFutureReservationsByUser() {
+
+	}
+
+	@Test
+	void testGetOngoingReservation() {
+
+	}
+
+	@Test
+	void testGetReservationsByParkAndDate() {
+
+	}
+
+	@Test
+	void testHasConcurrentReservation() {
+
+	}
+
+	@Test
+	void testSaveReservation() {
+
+	}
+}
