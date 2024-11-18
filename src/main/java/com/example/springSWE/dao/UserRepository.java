f621e80d43601c0849bda80388972ece1cbb4861
pietro <pietro.ciani@edu.unifi.it>
2024-11-18 12:56:08 +0100

    chore(user): delete comments

--- src/main/java/com/example/springSWE/dao/UserRepository.java
+++ src/main/java/com/example/springSWE/dao/UserRepository.java
@@ -7,7 +7,6 @@ import com.example.springSWE.model.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    //TODO: Optional<User> findById(Integer id);
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
