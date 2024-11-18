eca9f52dd089d2a8ad05254c082502d4926de97f
pietro <pietro.ciani@edu.unifi.it>
2024-11-18 20:12:28 +0100

    chores(depend): delete unused dependencies

--- src/test/java/com/example/springSWE/controller/SignupControllerTest.java
+++ src/test/java/com/example/springSWE/controller/SignupControllerTest.java
@@ -1,21 +1,16 @@
 package com.example.springSWE.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Optional;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
 import com.example.springSWE.dao.UserRepository;
 import com.example.springSWE.model.User;
-import com.example.springSWE.service.UserService;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -23,8 +18,6 @@ public class SignupControllerTest {
  
 	@Autowired
     private SignupController signupController;
-	@Autowired
-	private UserService userService;
 	@Autowired
 	private UserRepository userRepository;
 
--- src/test/java/com/example/springSWE/service/ReservationServiceTest.java
+++ src/test/java/com/example/springSWE/service/ReservationServiceTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.springSWE.dao.UserRepository;
