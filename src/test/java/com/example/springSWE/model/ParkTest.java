ce8d81d323fc3d338397ee3c5782baa8316ac6c8
pietro <pietro.ciani@edu.unifi.it>
2024-12-14 11:19:55 +0100

    test(park): add obj creation test

--- src/test/java/com/example/springSWE/model/ParkTest.java
+++ src/test/java/com/example/springSWE/model/ParkTest.java
@@ -2,11 +2,32 @@ package com.example.springSWE.model;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.time.LocalTime;
 
 public class ParkTest {
+
+	@Test
+    public void testParkObjectCreation() {
+        String name = "Central Park";
+        String address = "123 Park Avenue";
+        LocalTime openingTime = LocalTime.of(8, 0);
+        LocalTime closingTime = LocalTime.of(20, 0);
+
+        Park park = new Park();
+        park.setName(name);
+        park.setAddress(address);
+        park.setOpeningTime(openingTime);
+        park.setClosingTime(closingTime);
+
+        assertEquals(name, park.getName());
+        assertEquals(address, park.getAddress());
+        assertEquals(openingTime, park.getOpeningTime());
+        assertEquals(closingTime, park.getClosingTime());
+    }
+
 	@Test
 	void testIsOpen() {
 		Park park = new Park();
